df8738225605697cd6e4544cc147895fcc50eba8
QuiGon189 <leshka189@gmail.com>
2025-11-29 15:07:59 +0300

    update dto

--- services/schedule_service/internal/dto/schedule_dto.go
+++ services/schedule_service/internal/dto/schedule_dto.go
@@ -5,9 +5,14 @@ type AddGroupScheduleRequest struct {
 	HalfYear int `json:"half_year" example:"1"`
 	GroupName string `json:"group_name" example:"СА-501"`
 	Semester int `json:"semester" example:"5"`
-	ScheduleImgURL int `json:"schedule_ing_url" example:"https://example.com/schedule.jpg"`
+	ScheduleImgURL string `json:"schedule_img_url" example:"https://example.com/schedule.jpg"`
 }
+
+type 
 
 type GroupScheduleResponse struct {
-
+	GroupName string `json:"group_name" example:"СА-501"`
+	Semester int `json:"semester" example:"5"`
+	ScheduleImgURL string `json:"schedule_img_url" example:"https://example.com/schedule.jpg"`
 }
+
