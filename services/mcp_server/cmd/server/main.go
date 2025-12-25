8762ae8e2093aa54ff4fcb95a9819b1ea5a5afe6
QuiGon189 <leshka189@gmail.com>
2025-12-25 14:00:28 +0300

    update mcp

--- services/mcp_server/cmd/server/main.go
+++ services/mcp_server/cmd/server/main.go
@@ -1,40 +1,20 @@
 package main
 
 import (
-	"log"
 	"mcp_server/internal/config"
+	"mcp_server/internal/server"
 	"mcp_server/internal/tools"
-	"net/http"
 
-	"github.com/mark3labs/mcp-go/server"
+	"github.com/mark3labs/mcp-go/mcp"
 )
 
 func main() {
 	cfg := config.Load()
 
-	// ser := service.NewChangeService(cfg)
+	srv := server.NewMCPServer(cfg)
 
-	// changeHandler := tools.NewChangeHandler(ser)
-	// scheduleHandler := tools.NewSchedulesHandler(ser)
+	tgTools := tools.NewScheduleTools()
+	tgHandlers := tools.NewTGHandler(cfg)
 
-	tgHandler := tools.NewTGHandler(cfg)
-
-	mcpServer := server.NewMCPServer(
-		"Доступ к расписанию и изменениям расписания",
-		"1.0.0",
-		server.WithToolCapabilities(true),
-		server.WithLogging(),
-		server.WithRecovery(),
-	)
-
-	scheduleTools := tools.NewScheduleTools()
-	// mcpServer.AddTool(scheduleTools.GetChanges(), changeHandler.HandleChangeRequest)
-	// mcpServer.AddTool(scheduleTools.GetCurrentDate(), scheduleHandler.CurrentDateHandler)
-	// mcpServer.AddTool(scheduleTools.GetCurrentStudyPeriod(), scheduleHandler.CurrentStudyPeriodHandler)
-	// mcpServer.AddTool(scheduleTools.GetGroupSchedule(), scheduleHandler.GetGroupSchedule)
-	mcpServer.AddTool(scheduleTools.SendChanges(), tgHandler.SendChanges)
-
-	http.Handle("/mcp", server.NewStreamableHTTPServer(mcpServer))
-	log.Printf("MCP chedule server starting on :%s", cfg.ServerPort)
-	http.ListenAndServe(":"+cfg.ServerPort, nil)
+	srv.McpSrv.AddTool(tgTools.SendChanges(), mcp.NewStructuredToolHandler(tgHandlers.SendChanges))
 }
--- services/mcp_server/internal/tools/send_tg.go
+++ services/mcp_server/internal/tools/send_tg.go
@@ -6,7 +6,7 @@ import (
 	"mcp_server/internal/config"
 	"mcp_server/internal/models"
 	"mcp_server/internal/service"
-	"strconv"
+	"time"
 
 	"github.com/mark3labs/mcp-go/mcp"
 )
@@ -16,6 +16,17 @@ type TGHandler struct {
 	scheduleService *service.ChangeService
 }
 
+type SendChangesRequest struct {
+	ChatID int64     `json:"chat_id" jsonschema_description:"Идентификатор чата пользователя, необходимый для отправки сообщения" jsonschema:"required"`
+	Date   time.Time `json:"date" jsonschema_description:"Дата, на которую необходмо найти и отправить изменения в расписании" jsonschema:"required,format=date"`
+}
+
+type SendChangesResponse struct {
+	Success  bool `json:"success" jsonschema_description:"Сообщение доставленно пользователю"`
+	IsChange bool `json:"is_change" jsonschema_description:"Есть изменения на указанную дату"`
+	Message string `json:"message" jsonschema_description:"Сообщение MCP клиенту"`
+}
+
 func NewTGHandler(cfg *config.Config) *TGHandler {
 	return &TGHandler{
 		tgService:       service.NewTGService(cfg),
@@ -23,39 +34,42 @@ func NewTGHandler(cfg *config.Config) *TGHandler {
 	}
 }
 
-func (h *TGHandler) SendChanges(ctx context.Context, req mcp.CallToolRequest) (*mcp.CallToolResult, error) {
-	date, err := req.RequireString("date")
-	if err != nil {
-		log.Printf("Failed to get send changes date: %v", err)
-		return mcp.NewToolResultError("ошибка при проверке даты"), nil
-	}
-
-	chatIDs, err := req.RequireString("chat_id")
-	if err != nil {
-		log.Printf("Failed to get send changes chat_id: %v", err)
-		return mcp.NewToolResultError("ошибка при проверке chat_id"), nil
-	}
-
-	chatID, err := strconv.ParseInt(chatIDs, 10, 64)
-	if err != nil {
-		log.Printf("Failed to get send changes chat_id: %v", err)
-		return mcp.NewToolResultError("ошибка при проверке chat_id"), nil
-	}
-
-	changesResponse, err := h.scheduleService.GetChanges(date)
+func (h *TGHandler) SendChanges(ctx context.Context, req mcp.CallToolRequest, args SendChangesRequest) (SendChangesResponse, error) {
+	changesResponse, err := h.scheduleService.GetChanges(args.Date.Format("2005-02-01"))
 	if err != nil {
 		log.Printf("Failed to get changes: %v", err)
-		return mcp.NewToolResultError("ошибка при получении изменеий из сервиса"), nil
+		return SendChangesResponse{
+			Success: false,
+			IsChange: false,
+			Message: "Ошибка при получении данных от сервиса расписаний",
+		}, nil
+	}
+
+	if changesResponse.Data.Date == "" {
+		log.Print("Changes not found")
+		return SendChangesResponse{
+			Success: false,
+			IsChange: false,
+			Message: "Нет изменений на выбранную дату",
+		}, nil
 	}
 
 	if err := h.tgService.SendMessage(&models.SendMessage{
-		ChatID:    chatID,
+		ChatID:    args.ChatID,
 		Message:   changesResponse.Data.Description,
 		PhotoURLs: changesResponse.Data.ImgURLs,
 	}); err != nil {
 		log.Printf("Failed to send tg message: %v", err)
-		return mcp.NewToolResultError("ошибка при отправке сообщения в тг"), nil
+		return SendChangesResponse{
+			Success: false,
+			IsChange: true,
+			Message: "Изменения обнаружены, но произошла ошибка при отправке сообщения в телеграмм бот",
+		}, nil
 	}
 
-	return mcp.NewToolResultText("Пользователь получил информацию об изменениях"), nil
+	return SendChangesResponse{
+		Success: true,
+		IsChange: true,
+		Message: "Информация об изменениях отправленна пользователю по указанному chat_id",
+	} ,nil
 }
