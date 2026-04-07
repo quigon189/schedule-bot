aed066d2e14e95c7f7b8ebdf7e9a8dd1f1c91478
QuiGon189 <leshka189@gmail.com>
2026-04-07 18:26:10 +0300

    remove old services

--- services/bot_service/app/middlewaries/__init__.py
+++
@@ -1,4 +0,0 @@
-from .check_user import CheckUserMiddleware
-
-
-__all__ = ['CheckUserMiddleware']
--- services/schedule_service/internal/handlers/health.go
+++
@@ -1,8 +0,0 @@
-package handlers
-
-import "net/http"
-
-func HealthCheck(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.Write([]byte(`{"status": "healthy", "service": "schedule"}`))
-}
